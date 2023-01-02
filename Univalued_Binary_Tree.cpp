6df7329c4d653fb56d8a32af78708f61c7ab56b4
KOMAL SHARMA <92201633+Sharma-005komal@users.noreply.github.com>
2023-01-02 20:34:28 +0530

    Create Univalued_Binary_Tree.cpp

---
+++ Univalued_Binary_Tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnivalTree(TreeNode* root) {
+        int check = root->val;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* temp = q.front();
+            q.pop();
+            if(check != temp->val)
+            {
+                return false;
+            }
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+        return true;
+    }
+};
